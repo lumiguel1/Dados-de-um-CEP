ffa93f12077e705e8c1552a560794c3c5a24f2e2
ThiagoLozano <51161587+ThiagoLozano@users.noreply.github.com>
2020-08-19 11:57:18 -0300

    JS

--- Retornando Dados do CEP - WEB/funcoes.js
+++ Retornando Dados do CEP - WEB/funcoes.js
@@ -1,10 +1,32 @@
-function chamar() {
-    var captar = ""
-    captar = document.getElementById('cep').value;
-    if (captar.length != 8) {
-        alert("Erro: Tipo de Dado Inválido")
+function validar() {
+    // Pega o CEP e inseri na URL da API.
+    var cep = document.getElementById('cep');
+    var url = "https://api.postmon.com.br/v1/cep/" + cep.value;
+
+    // Pega os elementos de outros campos. 
+    var cidade = document.getElementById('cidade');
+    var bairro = document.getElementById('bairro');
+    var estado = document.getElementById('estado');
+    var logradouro = document.getElementById('logradouro');
+
+    // Valida o CEP.
+    if (cep.value.length != 8) {
+        alert("Erro: Tamanho de CEP inválido!")
     }
-    else {
-        document.getElementById('digito').innerHTML = "https://api.postmon.com.br/v1/cep/" + captar
+    else if(isNaN(parseInt(cep.value))){
+        alert("Erro: Tipo de Dado inválido")
+    }
+    else{
+        // Faz o Request da API.
+        fetch(url, { method: 'GET' })
+            .then(function (response) {
+                response.json()
+                    .then(function (dado) {
+                        cidade.value = dado.cidade
+                        bairro.value = dado.bairro
+                        estado.value = dado.estado
+                        logradouro.value = dado.logradouro
+                    });
+            })
     }
 }
